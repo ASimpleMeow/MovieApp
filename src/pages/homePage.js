898bca494aeaa23a63e2e42d69c21766a63a135b
Oleksandr Kononov <oleksandr.kononov@errigal.com>
2020-02-11 09:30:47 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
+import StubAPI from "../api/stubAPI";
 
 const MovieListPage = () => {
   const [titleFilter, setTitleFilter] = useState("");
@@ -36,11 +37,21 @@ const MovieListPage = () => {
     else setGenreFilter(value);
   };
 
+  const addToFavorites = movieId => {
+    setMovies(movies => {
+      const index = movies.map(m => m.id).indexOf(movieId);
+      StubAPI.add(movies[index]);
+      let newMoviesState = [...movies]
+      newMoviesState.splice(index, 1);
+      return newMoviesState;
+    });
+  };
+
   return (
     <>
       <Header numMovies={displayedMovies.length} />
       <FilterControls onUserInput={handleFilterChange}/>
-      <MovieList movies={displayedMovies} />
+      <MovieList movies={displayedMovies} buttonHandler={addToFavorites}/>
     </>
   );
 };
